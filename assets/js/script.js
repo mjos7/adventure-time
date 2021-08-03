d201d43f3d81ed9d88eaf11b70db2dafe2f95160
mjos7 <markjnjoseph@gmail.com>
2021-08-02 21:45:36 -0700

    update local storage and and input transparency

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,50 +1,78 @@
-
-
 // recent search boxes
 
-var newRecentDate = document.querySelector("#date");
-var newRecentTime = document.querySelector("#time");
-var newRecentCity = document.querySelector("#location");
-var searchItemEl = document.querySelector("#search-item");
-var searchItemCard = document.querySelector("#recent-searches");
-var searchBtn = document.getElementById("search");
-var recentSearchArray = [];
+var newRecentDate = document.querySelector('#date');
+var newRecentTime = document.querySelector('#time');
+var newRecentCity = document.querySelector('#location');
+var searchItemEl = document.querySelector('#search-item');
+var searchItemCard = document.querySelector('#recent-searches');
+var searchBtn = document.getElementById('search');
+var recentSearchArray = JSON.parse(localStorage.getItem('searches')) || [];
 
-var loadHistory =  function () {
-    searchArray = JSON.parse (localStorage.getitem(""));
+var loadHistory = function () {
+  searchArray = JSON.parse(localStorage.getitem(''));
 
-    if (searchArray) {
-        searchArray = JSON.parse (localStorage.getitem(""));
-        for (let i = 0; i < searchArray.length; i++) {
-            var newRecentSearch = document.createElement("button");
-            newRecentSearch.className = "search-item";
-            newRecentSearch.setAttribute("", searchArray[i])
-            newRecentSearch.innerHTML = searchArray[i];
-            searchItemEl.appendChild(newRecentSearch);
-            searchItemCard.removeAttribute("style");
-        }
+  if (searchArray) {
+    searchArray = JSON.parse(localStorage.getitem(''));
+    for (let i = 0; i < searchArray.length; i++) {
+      var newRecentSearch = document.createElement('button');
+      newRecentSearch.className = 'search-item';
+      newRecentSearch.setAttribute('', searchArray[i]);
+      newRecentSearch.innerHTML = searchArray[i];
+      searchItemEl.appendChild(newRecentSearch);
+      searchItemCard.removeAttribute('style');
     }
-
-
-}
+  }
+};
 
 // Search weather using search history buttons
 var buttonClickHandler = function (event) {
-    var cityname = event.target.getAttribute("");
-    if (cityname) {
-        getWeatherInfo(cityname);
-    }
-}
+  var tempArray = JSON.parse(localStorage.getItem('searches')) || [];
+  var cityname = event.target.getAttribute('');
+  if (cityname) {
+    getWeatherInfo(cityname);
+  }
+};
 
-searchBtn.addEventListener("click", function (event) {
-    event.preventDefault();
-    var recentDate = newRecentDate.value;
-    var recentTime = newRecentTime.value;
-    var recentCity = newRecentCity.value;
-    var recentSearchObj = {
-        date: recentDate,
-        time: recentTime,
-        city: recentCity
-    }
-    console.log(recentSearchObj);
-})
+searchBtn.addEventListener('click', function (event) {
+  event.preventDefault();
+
+  var recentDate = newRecentDate.value;
+  var recentCity = newRecentCity.value;
+
+  var recentSearchObj = {
+    date: recentDate,
+    city: recentCity,
+  };
+  console.log(recentSearchObj);
+  localStorage.setItem('searches', JSON.stringify(tempArray));
+  recentSearchArray.push(recentSearchObj);
+
+  //   // clear old content
+  //   $(document).ready(function () {
+  //     $(newRecentCity).val('');
+  //   });
+});
+
+// Mark's old code
+
+// When user inputs City and submits
+// $('#search-form').submit(function (event) {
+//   event.preventDefault();
+//   var city = searchBarEl.value.trim();
+//   if (city == '') {
+//     alert('Please enter a city');
+//     return;
+//   } else {
+//     $('html, body').animate(
+//       {
+//         scrollTop: $('#city-name').offset().top - 70,
+//       },
+//       500
+//     );
+//     startWeather(city);
+//   }
+//   // clear old content
+//   $(document).ready(function () {
+//     $(searchBarEl).val('');
+//   });
+// });
