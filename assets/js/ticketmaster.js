688ec044dab1b5241b9cfcf9f333b33b34d00d90
Cayman Gill <cayman.g@hotmail.com>
2021-08-03 20:57:00 -0400

    fixed issues with local branch

--- assets/js/ticketmaster.js
+++ assets/js/ticketmaster.js
@@ -14,13 +14,13 @@ function eventChoice(events){
 }
 
 // on click search fetces data. choice and hopefully start date can be passed without any issue.
-    var startDate = $("#start").val();
-    var endDate = $("#start").val();
-    var city = $("#city-input").val();
+    // var startDate = $("#start").val();
+    // var endDate = $("#start").val();
+    // var city = $("#city-input").val();
 
-    console.log(startDate);
-    console.log(endDate);
-    console.log(city);
+    // console.log(startDate);
+    // console.log(endDate);
+    // console.log(city);
 
         fetch(`https://app.ticketmaster.com/discovery/v2/events?&keyword=${eventChoice(events)}&localStartDate=${startDate}&localEndDateTime=${endDate}&city=${city}&apikey=7elxdku9GGG5k8j0Xm8KWdANDgecHMV0`)
         .then(response => {
@@ -56,6 +56,13 @@ function eventChoice(events){
                 ticketmasterData.country = event._embedded.venues[0].country.countryCode;
                 ticketmasterData.lat = event._embedded.venues[0].location.latitude;
                 ticketmasterData.long = event._embedded.venues[0].location.longitude;
-                console.log(ticketmasterData);
+
+                $("#event-title").text(ticketmasterData.name);
+                $("#event-address h2").text(`${ticketmasterData.street} ${ticketmasterData.city}, ${ticketmasterData.state}`);
+                $("#event-link a").attr("href", ticketmasterData.url);
+                $("#event-image").attr("src", ticketmasterData.img);
+          
+                console.log(ticketmasterData.lat+ ", " + ticketmasterData.long)
+                search(ticketmasterData.lat, ticketmasterData.long);
             }       
         });
