35917d0d297888f0729ae07382ca280b1eb9579e
Catherine <catheneghan@gmail.com>
2021-08-07 18:49:48 +0100

    changes to formsubmitHandler function

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,29 +15,30 @@ var recentSearchesEl = $('.recent-searches');
 var searchBtnEl = $('#search');
 
 var searchHistoryArray = JSON.parse(localStorage.getItem('citySearch')) || [];
-function formSubmitHandler() {
-  event.preventDefault();
-  // get city name value from input element
-  var city = $('#location').val();
-  var date = $('#inline_cal').val();
 
-  // Set city name in local storage and generate history buttons
-  if (city) {
-    if (searchHistoryArray.length <= 7) {
-      searchHistoryArray.push({ city: city, date: date });
-    } else if (searchHistoryArray.length > 5) {
-      searchHistoryArray.pop();
-      searchHistoryArray.splice(0, 0, { city: city, date: date });
-    }
+// function formSubmitHandler() {
+//   event.preventDefault();
+//   // get city name value from input element
+//   var city = $('#location').val();
+//   var date = $('#inline_cal').val();
 
-    sessionStorage.setItem(
-      'search',
-      JSON.stringify({ city: city, date: date })
-    );
-  }
-  localStorage.setItem('citySearch', JSON.stringify(searchHistoryArray));
-  location.href = './adventure.html';
-}
+//   // Set city name in local storage and generate history buttons
+//   if (city) {
+//     if (searchHistoryArray.length <= 7) {
+//       searchHistoryArray.push({ city: city, date: date });
+//     } else if (searchHistoryArray.length > 5) {
+//       searchHistoryArray.pop();
+//       searchHistoryArray.splice(0, 0, { city: city, date: date });
+//     }
+
+//     sessionStorage.setItem(
+//       'search',
+//       JSON.stringify({ city: city, date: date })
+//     );
+//   }
+//   localStorage.setItem('citySearch', JSON.stringify(searchHistoryArray));
+//   location.href = './adventure.html';
+// }
 // Load any past city searches
 function loadHistory() {
   for (var i = searchHistoryArray.length - 1; i >= 0; i--) {
