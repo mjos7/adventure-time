7b60fb06ddca591d8560cb8dc33e0f5807c5e966
mjos7 <83667780+mjos7@users.noreply.github.com>
2021-08-14 12:20:27 -0700

    fix map height: :

---
+++ tailwind.config 2.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+// ADD THIS AT END OF PROJECT
+// const purge = process.eng.NODE_ENV === 'production' ? true : false;
+// purge: { enabled: true} { content: ['./build/**/*.html']};
